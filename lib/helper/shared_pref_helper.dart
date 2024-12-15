408725f64553bed5d39d95d960711d817d9ce4c4
Aasim Sayyed <aasimsayyed@Aasims-MacBook-Air.local>
2024-12-15 16:11:46 +0530

    shared pref

--- lib/helper/shared_pref_helper.dart
+++ lib/helper/shared_pref_helper.dart
@@ -1,21 +1,63 @@
 import 'package:shared_preferences/shared_preferences.dart';
 
-class SharedPrefHelper {
-  // Method to save data to SharedPreferences
-  static Future<void> saveData(String key, String value) async {
-    final prefs = await SharedPreferences.getInstance();
-    await prefs.setString(key, value);
+class SharedPreferencesUtils {
+  static late SharedPreferences
+      _preferences; // Singleton pattern to ensure only one instance of the class is created
+  static final SharedPreferencesUtils _instance =
+      SharedPreferencesUtils._internal();
+  factory SharedPreferencesUtils() {
+    return _instance;
+  }
+  SharedPreferencesUtils._internal();
+  static Future<void> init() async {
+    _preferences = await SharedPreferences.getInstance();
   }
 
-  // Method to retrieve data from SharedPreferences
-  static Future<String?> getData(String key) async {
-    final prefs = await SharedPreferences.getInstance();
-    return prefs.getString(key);
+  Future<bool> setString(String key, String value) async {
+    return await _preferences.setString(key, value);
   }
 
-  // Method to remove data from SharedPreferences
-  static Future<void> removeData(String key) async {
-    final prefs = await SharedPreferences.getInstance();
-    await prefs.remove(key);
+  String? getString(String key) {
+    return _preferences.getString(key);
+  }
+
+  Future<bool> setInt(String key, int value) async {
+    return await _preferences.setInt(key, value);
+  }
+
+  int? getInt(String key) {
+    return _preferences.getInt(key);
+  }
+
+  Future<bool> setDouble(String key, double value) async {
+    return await _preferences.setDouble(key, value);
+  }
+
+  double? getDouble(String key) {
+    return _preferences.getDouble(key);
+  }
+
+  Future<bool> setBool(String key, bool value) async {
+    return await _preferences.setBool(key, value);
+  }
+
+  bool? getBool(String key) {
+    return _preferences.getBool(key);
+  }
+
+  Future<bool> setStringList(String key, List<String> value) async {
+    return await _preferences.setStringList(key, value);
+  }
+
+  List<String>? getStringList(String key) {
+    return _preferences.getStringList(key);
+  }
+
+  Future<bool> remove(String key) async {
+    return await _preferences.remove(key);
+  }
+
+  Future<bool> clear() async {
+    return await _preferences.clear();
   }
 }
